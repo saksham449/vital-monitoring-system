e25116c7a27cfb2dda48499c158c936823e35ce3
Saksham Mittal <mittalsaksham967@gmail.com>
2026-09-04 13:26:01 +0100

    feat: configure STOMP WebSocket messaging

---
+++ backend/src/main/java/com/saksham/vitalmonitoring/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.saksham.vitalmonitoring.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final String frontendUrl;
+
+    public WebSocketConfig(@Value("${frontend.url}") String frontendUrl) {
+        this.frontendUrl = frontendUrl;
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns(frontendUrl);
+    }
+}
